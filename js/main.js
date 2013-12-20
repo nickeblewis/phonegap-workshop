7a559e8b54457fe9f5dfa7dc5465939d2bc208ea
Nick Lewis <nickeblewis@gmail.com>
2013-12-20 23:42:33 +0000

    step 7

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 var app = {
 
-    renderHomeView: function() {
-     $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    
-},
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -13,24 +8,12 @@ var app = {
         }
     },
 
-    findByName: function() {
-        var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-    },
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    },
-
-
+    }
 
 };
 
