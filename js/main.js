2e93a5a0961c0aea7a284ef447087a8149e0d627
Nick Lewis <nickeblewis@gmail.com>
2013-12-20 23:28:53 +0000

    part 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 var app = {
+
+    renderHomeView: function() {
+     $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    
+},
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -8,24 +14,20 @@ var app = {
     },
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+        var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
     },
 
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store initialized', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
 
